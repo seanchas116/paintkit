0c3a368d27bd1d2ff13dcc8a16c0c7dc60c1c076
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-13 19:59:05 +0900

    Fix jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
-module.exports = {
-  rootDir: "src",
+export default {
   preset: "ts-jest",
   testEnvironment: "node",
   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
 };
