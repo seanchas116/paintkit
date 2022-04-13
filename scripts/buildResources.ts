a87423d28a34aa33c6322005e396b4bf8e202651
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-13 17:16:51 +0900

    Add resource build script

---
+++ scripts/buildResources.ts
@@ -0,0 +1,51 @@
+import path from "path";
+import fs from "fs";
+import util from "util";
+import _glob from "glob";
+import dedent from "dedent";
+import { ElementNode, parse } from "svg-parser";
+
+const glob = util.promisify(_glob);
+
+async function generateIcons(cwd: string): Promise<void> {
+  const files = await glob("**/*.svg", { cwd });
+  for (const file of files) {
+    const filePath = path.resolve(cwd, file);
+    const basePath = filePath.slice(0, -4);
+    const svgText = fs.readFileSync(filePath, { encoding: "utf-8" });
+    const rootNode = parse(svgText);
+    const svgNode = rootNode.children[0] as ElementNode;
+
+    const width = Number(svgNode.properties!.width);
+    const height = Number(svgNode.properties!.height);
+    const body =
+      '<g fill="none">' +
+      svgText
+        .replace(/<svg[^>]*>/, "")
+        .replace("</svg>", "")
+        .replaceAll(/#(?:[0-9a-fA-F]{3}){1,2}/g, "currentColor") +
+      "</g>";
+
+    fs.writeFileSync(
+      `${basePath}.ts`,
+      dedent`
+        import { IconifyIcon } from '@iconify/types';
+        const data: IconifyIcon = {
+          body: \`${body}\`,
+          width: ${width},
+          height: ${height},
+        };
+        export default data;
+      `
+    );
+  }
+}
+
+const packageDir = path.dirname(__dirname);
+const iconsDir = path.resolve(packageDir, "src/icon");
+
+async function run() {
+  await generateIcons(iconsDir);
+}
+
+run().finally(() => {});
