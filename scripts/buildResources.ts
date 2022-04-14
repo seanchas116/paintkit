f821e17f05438cabf246cacd12a884571fcbf1c6
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-14 13:28:53 +0900

    Embed css files automatically

--- scripts/buildResources.ts
+++ scripts/buildResources.ts
@@ -2,9 +2,12 @@ import path from "path";
 import fs from "fs";
 import util from "util";
 import url from "url";
+import { createRequire } from "module";
 import _glob from "glob";
 import dedent from "dedent";
 import { ElementNode, parse } from "svg-parser";
+
+const require = createRequire(import.meta.url);
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
@@ -44,11 +47,38 @@ async function generateIcons(cwd: string): Promise<void> {
   }
 }
 
+async function generateCSSStrings(cwd: string): Promise<void> {
+  const files = {
+    simpleBar: "simplebar/dist/simplebar.min.css",
+    tippy: "tippy.js/dist/tippy.css",
+    minireset: "minireset.css/minireset.min.css",
+  };
+
+  const contents: Record<string, string> = {};
+
+  for (const [name, importPath] of Object.entries(files)) {
+    const filePath = require.resolve(importPath);
+    const content = fs.readFileSync(filePath, { encoding: "utf-8" });
+    contents[name] = content;
+  }
+
+  fs.writeFileSync(
+    `${path.resolve(cwd, "src/components/LibraryCSS")}.ts`,
+    Object.entries(contents)
+      .map(
+        ([name, content]) =>
+          `export const ${name} = ${JSON.stringify(content)};`
+      )
+      .join("\n")
+  );
+}
+
 const packageDir = path.dirname(__dirname);
 const iconsDir = path.resolve(packageDir, "src/icon");
 
 async function run() {
   await generateIcons(iconsDir);
+  await generateCSSStrings(packageDir);
 }
 
 run().finally(() => {});
