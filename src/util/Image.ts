e525336b7d9840dca4f446557d2bc8088845002d
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-18 17:51:42 +0900

    Fix @iconify/build import

--- src/util/Image.ts
+++ src/util/Image.ts
@@ -2,7 +2,7 @@ import { Vec2 } from "paintvec";
 import parse from "html-react-parser";
 import React from "react";
 import { IconifyIcon } from "@iconify/types";
-import { iconToSVG } from "@iconify/utils";
+import { iconToSVG } from "@iconify/utils/lib/svg/build";
 
 // 1x1 PNG filled with rgba(255, 0, 0, 0.5)
 export const emptyPNGDataURL =
