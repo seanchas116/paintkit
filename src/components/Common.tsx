209f07364dae0cfbb00d21219ce3781a57ba476d
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-26 15:41:06 +0900

    monospaceFontFamily

--- src/components/Common.tsx
+++ src/components/Common.tsx
@@ -191,3 +191,4 @@ export const downIconStyle = css`
 `;
 
 export const fontFamily = `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`;
+export const monospaceFontFamily = `"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace`;
