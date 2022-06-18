1d42e15994900ecbdaede64449b4e137a67f95fb
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-18 22:25:13 +0900

    Tweak

--- src/util/MessageProxy.test.ts
+++ src/util/MessageProxy.test.ts
@@ -1,19 +1,19 @@
 import { describe, it, expect } from "vitest";
 import { establishIPC, Endpoint } from "./MessageProxy";
 
-const methodsA = {
-  async increment(value: number) {
-    return value + 1;
-  },
-};
-const methodsB = {
-  async decrement(value: number) {
-    return value - 1;
-  },
-};
-
 describe(establishIPC.name, () => {
   it("should call method on handler", async () => {
+    const methodsA = {
+      async increment(value: number) {
+        return value + 1;
+      },
+    };
+    const methodsB = {
+      async decrement(value: number) {
+        return value - 1;
+      },
+    };
+
     const endpointA: Endpoint = {
       addEventListener(handler: (data: any) => void): () => void {
         endpointB.postMessage = handler;
@@ -33,7 +33,7 @@ describe(establishIPC.name, () => {
     const a = establishIPC<typeof methodsA>(methodsB, endpointB);
     const b = establishIPC<typeof methodsB>(methodsA, endpointA);
 
-    const ret = await b.decrement(2);
-    expect(ret).toBe(1);
+    expect(await a.increment(2)).toBe(3);
+    expect(await b.decrement(2)).toBe(1);
   });
 });
